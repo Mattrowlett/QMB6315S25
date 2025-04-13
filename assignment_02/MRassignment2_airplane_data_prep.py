8f920ec01311072562cacb76ebc96958716f5ef3
Mattrowlett <ma608480@ucf.edu>
2025-04-12 22:46:24 -0400

    assignment 2
    assignment 2

--- assignment_02/MRassignment2_airplane_data_prep.py
+++ assignment_02/MRassignment2_airplane_data_prep.py
@@ -18,10 +18,14 @@
 
 ##################################################
 # Import Required Modules
-##################################################
-import pandas as pd
-import statsmodels.api as sm
+################################################
 
+import pandas as pd
+import os
+
+
+# Import a module for estimating regression models.
+import statsmodels.formula.api as smf # Another way to estimate linear regression
 
 ##################################################
 # Set up Workspace
@@ -52,53 +56,64 @@ os.getcwd()
 # Part a) Read Spreadsheet and Sales Data
 ##################################################
 
-
-
+airplane_sales = pd.read_excel('airplane_data.xlsx', sheet_name='airplane_sales')
+excel_file = pd.ExcelFile('airplane_data.xlsx')
+print(excel_file.sheet_names)
+print("Question 1b: Summary of airplane_sales")
+print(airplane_sales.describe())
 
 #--------------------------------------------------
 # Fit a regression model.
 #--------------------------------------------------
-
+reg_model_sales = smf.ols('price ~ age', data=airplane_sales).fit()
+print("\nQuestion 1c: Regression model price ~ age")
+print(reg_model_sales.summary())
 
 
 
 ##################################################
 # Part b) Read Specification Data
 ##################################################
-
-
+# b) Summary of the data
+airplane_specs = pd.read_excel('airplane_data.xlsx', sheet_name='airplane_specs')
 
 #--------------------------------------------------
 # Join the two datasets together.
 #--------------------------------------------------
-
-
+airplane_sales_specs = pd.merge(airplane_sales, airplane_specs, on='sale_id', how='inner')
+print("\nQuestion 2c: Summary of airplane_sales_specs")
+print(airplane_sales_specs.describe())
 
 
 #--------------------------------------------------
 # Fit another regression model.
 #--------------------------------------------------
-
-
-
+reg_model_sales_specs = smf.ols('price ~ age + passengers + wtop + fixgear + tdrag', 
+                                data=airplane_sales_specs).fit()
+print("\nQuestion 2d: Regression model price ~ age + passengers + wtop + fixgear + tdrag")
+print(reg_model_sales_specs.summary())
 
 ##################################################
 # Part c) Read Performance Data
 ##################################################
-
-
+airplane_perf = pd.read_excel('airplane_data.xlsx', sheet_name='airplane_perf')
 
 #--------------------------------------------------
 # Join the third dataset to the first two.
 #--------------------------------------------------
 
-
-
+airplane_full = pd.merge(airplane_sales_specs, airplane_perf, on='sale_id', how='inner')
+print("\nQuestion 3c: Summary of airplane_full")
+print(airplane_full.describe())
 
 #--------------------------------------------------
 # Fit another regression model.
 #--------------------------------------------------
 
+reg_model_full = smf.ols('price ~ age + passengers + wtop + fixgear + tdrag + horse + fuel + ceiling + cruise', 
+                         data=airplane_full).fit()
+print("\nQuestion 3d: Regression model price ~ age + passengers + wtop + fixgear + tdrag + horse + fuel + ceiling + cruise")
+print(reg_model_full.summary())
 
 
 ##################################################
